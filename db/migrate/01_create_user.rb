c86e0fd57688ebd60e92f80144522ed7090cbba1
Sam. O <samrey2018@gmail.com>
2018-05-21 17:15:05 +0000

    automatically backed up by learn

--- db/migrate/01_create_user.rb
+++ db/migrate/01_create_user.rb
@@ -1,6 +1,6 @@
 class CreateUser < ActiveRecord::Migration[5.1]
 
-  # create a table using the Rake T.... installed.
+  # create a table using the Rake T.... under activerecord.
     def up
         create_table :users do |t|
             t.string :username
